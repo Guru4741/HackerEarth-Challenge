2c4fd3986e38ba318c28ecbdd1054ccf7911dfd1
Gaurav Choudhary <gaurav4741@gmail.com>
2019-06-04 12:04:54 +0530

    Search Feature Added

--- client/main.js
+++ client/main.js
@@ -1,17 +1,26 @@
 //Getting DOM Elements
 const ispNames = document.querySelector('.ispNames');
 const ispInfo = document.querySelector('.ispInfo');
+const btnDownload = document.querySelector('.buttons__download');
+const searchBox = document.querySelector('.searchBox');
+
+//Array to store ISP list
+const ISPs = [];
 
 //Fetch Call to API
 fetch('http://localhost:7676')  //Server is running on port 7676
     .then(data => data.json())
     .then(res => {
-        console.log(res);
+
         res.forEach(each => {
             listISPNames(each);
+            ISPs.push(each);
         })
     })
     .catch(err => console.log(err));
+
+
+// ISPs.forEach(each => listISPNames(each))
 
 function listISPNames(e) {
 
@@ -44,6 +53,31 @@ function listISPNames(e) {
     ispNames.appendChild(el);
 }
 
+//Attaching Event to Search Box
+searchBox.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        const searchKeyword = searchBox.value.toLowerCase();
+        const searchFound = ISPs.filter(each => each.name.toLowerCase() === searchKeyword);
+        if (searchFound.length !== 0) {
+            console.log(searchFound[0])
+            ispNames.innerHTML = "";
+            listISPNames(searchFound[0]);
+        } else if (searchKeyword === "") {
+            ispNames.innerHTML = "";
+            ISPs.forEach(each => listISPNames(each))
+        } else {
+            ispNames.innerHTML = "No Such Provider exists."
+        }
+    }
+})
+
+//Attaching Event Download Button
+btnDownload.addEventListener('click', () => {
+    downloadPDF()
+})
+
+
+//ISP list display function
 function displayISP(e) {
     const ispName = document.querySelector('.ispInfoName');
     const ispImage = document.querySelector('.ispImage');
@@ -55,4 +89,25 @@ function displayISP(e) {
     ispRatings.innerText = `Ratings: ${e.ratings}`;
     ispDescription.innerText = e.desc;
 
+}
+
+//ISP Download function
+function downloadPDF() {
+    const ispName = document.querySelector('.ispInfoName');
+    const isp = ISPs.filter(each => each.name === ispName.innerText);
+
+    fetch('http://localhost:7676/download', {
+        method: "POST",
+        body: JSON.stringify({ "ISP": isp }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(data => data.blob())
+        .then(blob => URL.createObjectURL(blob))
+        .then(url => {
+            window.open(url, '_blank');
+            URL.revokeObjectURL(url);
+        })
+        .catch(err => console.log(err));
 }
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const path = require('path');
+const fs = require('fs');
 
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const PDFDocument = require('pdfkit');
 const express = require('express');
 const app = express();
 
 const data = require('./public/data');
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.json(data);
+})
+
+app.post('/download', (req, res) => {
+    const data = req.body.ISP[0];
+    const doc = new PDFDocument;
+
+    res.setHeader('Content-Disposition', 'attachment; filename=ISPDetails.pdf');
+    res.setHeader('Content-Type', 'application/pdf');
+
+    doc.image(`./public${data.imageURL}`, 250, 20, { scale: 0.25 });
+    doc.fillColor("black")
+        .text(`Provider : ${data.name}`, 100, 100)
+        .text(`Price : ${data.price}`, 100, 120)
+        .text(`Speed : ${data.speed}`, 100, 140)
+        .text(`Contact No.: ${data.phone}`, 100, 160)
+        .text(`Email : ${data.email}`, 100, 180)
+        .text(`Details : ${data.desc}`, 100, 200)
+    doc.pipe(res);
+    doc.end();
+
 })
 
 app.listen(7676, () => {
